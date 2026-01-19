803d6077cf28777b4a53767d27fb3a83aa0660eb
tavo-22 <tavs0355@gmail.com>
2026-01-19 10:53:06 -0500

    Proyecto inical Spring Boot - API Indicacores Economicos Colombia

---
+++ src/main/java/com/gustavo/api_indicadores_economicos_colombia/ApiIndicadoresEconomicosColombiaApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.api_indicadores_economicos_colombia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIndicadoresEconomicosColombiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIndicadoresEconomicosColombiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/api_indicadores_economicos_colombia/ApiIndicadoresEconomicosColombiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.api_indicadores_economicos_colombia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIndicadoresEconomicosColombiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
